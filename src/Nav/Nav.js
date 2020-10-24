a88d47b9f1d957e3e5cf8a6861af175fd3af0e5c
gsarbaj <g.sarbaj@icloud.com>
2020-10-24 22:36:14 +0300

    Nav Bar Added

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -2,19 +2,20 @@ import React from 'react';
 import Logo from "../Logo/Logo";
 import MarkoLogo from '../img/logoMarko.png'
 import {NavLink} from "react-router-dom";
+import classes from './Nav.module.css'
 
-const Nav = props => {
+const Nav = () => {
     return(
 
-        <div>
+        <div className={classes.nav}>
             <div>
                 <Logo logo={MarkoLogo} alt={'Обувь оптом от производителя'} width={'217'} height={'48'}/>
                 <input type="search"/>
                 <ul>
-                    <li><div></div><div>Личный кабинет</div></li>
-                    <li><div></div><div>Избранное</div></li>
-                    <li><div></div><div>Заказы</div></li>
-                    <li><div></div><div>380500</div></li>  {/*Указывается цена со знаком валюты и значек грузовика а не карзины*/}
+                    <li><NavLink to={'/'}><div></div><div>Личный кабинет</div></NavLink></li>
+                    <li><NavLink to={'/'}><div></div><div>Избранное</div></NavLink></li>
+                    <li><NavLink to={'/'}><div></div><div>Заказы</div></NavLink></li>
+                    <li><NavLink to={'/'}><div></div><div>380500</div></NavLink></li>  {/*Указывается цена со знаком валюты и значек грузовика а не карзины*/}
                 </ul>
             </div>
             <div>
@@ -25,7 +26,7 @@ const Nav = props => {
                         <li><NavLink to={'/'}>Детская обувь</NavLink></li>
                         <li><NavLink to={'/'}>Спортивная обувь</NavLink></li>
                         <li><NavLink to={'/'}>Новинки</NavLink></li>
-                        <li><NavLink to={'/'}>Акция</NavLink></li>
+                        <li><NavLink to={'/'}>Акции</NavLink></li>
                     </ul>
                 </nav>
             </div>
