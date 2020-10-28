13fcabf1e294025c7b29b34828d07f1c61e6e5fe
MariaSarbaj <marysarbaj@gmail.com>
2020-10-28 21:13:41 +0200

    css

--- src/PicText/PicText.js
+++ src/PicText/PicText.js
@@ -3,10 +3,10 @@ import classes from './PicText.module.css'
 
 const PicText = props => {
     return(
-        <div>
+        <div className={classes.PicText__wrapper}>
             <h3 className={classes.PicText__title}>{props.title}</h3>
             <span className={classes.PicText__text}>{props.text}</span>
-            <img className={classes.PicText__img} src={props.src} alt={props.alt}/>
+            <img className={classes.PicText__img} src={props.src} alt={props.alt} width={550}/>
         </div>
     )
 }
